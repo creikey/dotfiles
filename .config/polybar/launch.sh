9f4e10da6c2a4aa3d29fe3e755e9fb4cc9a5d5ff
Cameron Reikes <cameronreikes@gmail.com>
2019-11-23 04:19:28 -0800

    Improve polybar and add per monitor workspaces

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 >>/tmp/polybar1.log 2>&1 &
+polybar bar1 >>/tmp/polybarbase.log 2>&1 &
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar bar2 >>/tmp/polybar$m.log 2>&1 &
+done
 
 echo "Bars launched..."
